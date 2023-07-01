88dae2c8c745a527dcc102e7db184559112f55bd
eliot <contact@eliotmasset.fr>
2023-07-01 19:10:07 +0200

    [UPDATE]

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,10 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   showTable: boolean = false;
+  clientId: string = '8179d27b0dc985ca3a43';
+  redirectUri: string = encodeURIComponent('http://localhost:8090/callback');
+  githubUrl: string = `https://github.com/login/oauth/authorize?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&scope=read:user`;
+
 
   onSearch() {
     this.showTable = true;
+  }
+
+  onRegisterGitHub() {
+    window.open("http://localhost:8090/login");
   }
 }
